055f164a1f83b12ae3cd538fa422073b105e957c
leanhduy <laduy.hcmus@gmail.com>
2023-04-21 14:15:25 +0700

    TRC-10 add apis and tests for built-in User model

--- backend/account/serializers.py
+++ backend/account/serializers.py
@@ -1,7 +1,14 @@
 """This file contains all the serializers for the account app
 """
+from django.contrib.auth.models import User
 from rest_framework import serializers
 from account.models import Profile
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ("id", "username", "email", "first_name", "last_name")
 
 
 class ProfileSerializer(serializers.ModelSerializer):
